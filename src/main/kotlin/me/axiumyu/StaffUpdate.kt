44a47341ebb59f94124e6db1c4b11c1a5d41056c
Axiumyu <1738884091@qq.com>
2024-09-29 14:08:05 +0800

    优化快速升级

--- src/main/kotlin/me/axiumyu/StaffUpdate.kt
+++ src/main/kotlin/me/axiumyu/StaffUpdate.kt
@@ -1,7 +1,5 @@
 package me.axiumyu
 
-import me.axiumyu.Staff.Companion.FOOD
-import me.axiumyu.Staff.Companion.LEVEL
 import me.axiumyu.Staff.Companion.TAG
 import me.axiumyu.WindStaffRemake.Companion.xc
 import net.kyori.adventure.text.Component.text
@@ -14,12 +12,6 @@ import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemRarity
 import org.bukkit.persistence.PersistentDataType
-import kotlin.math.E
-import kotlin.math.exp
-import kotlin.math.log
-import kotlin.math.max
-import kotlin.math.min
-import kotlin.math.pow
 import kotlin.math.round
 import kotlin.random.Random
 
@@ -45,34 +37,46 @@ class StaffUpdate : CommandExecutor {
             return false
         }
         var allCost = 0.0
+        val oldLevel = item.itemMeta.getEnchantLevel(Enchantment.FEATHER_FALLING)
+        var newLevel = oldLevel
+        val oldFood = item.itemMeta.getEnchantLevel(Enchantment.PUNCH)
+        var newFood = oldFood
         for (i in 1..upTo) {
-            val level = item.itemMeta.getEnchantLevel(Enchantment.FEATHER_FALLING)
-            val food = item.itemMeta.getEnchantLevel(Enchantment.PUNCH)
-            if (level < 61 && xc.getPlayerData(p0.uniqueId).balance >= getUpCost(level).toBigDecimal()) {
-                xc.changePlayerBalance(p0.uniqueId, p0.name, getUpCost(level).toBigDecimal(), false)
-                allCost += getUpCost(level)
-                item.editMeta {
-                    if (level.mod(3) == 0 && food > 1) {
-                        it.addEnchant(Enchantment.PUNCH, food - 1, true)
-                    }
-                    it.addEnchant(Enchantment.FEATHER_FALLING, level + 1, true)
+            if (newLevel < 61 && xc.getPlayerData(p0.uniqueId).balance >= getUpCost(newLevel).toBigDecimal()) {
+                allCost += getUpCost(newLevel)
+                if (newLevel.mod(3) == 0 && newFood > 1) {
+                    newFood -= 1
                 }
-            } else if (level>= 61) {
+                newLevel += 1
+            } else if (newLevel >= 61) {
                 p0.sendActionBar(text("已经达到最高等级！").color(color(0xffea3a)))
-            }else{
-                p0.sendActionBar(text("你的账户余额只够升级到 $level 级！").color(color(0xffea3a)))
+                break
+            } else {
+                p0.sendActionBar(
+                    text(
+                        "你的账户余额只够升级到 $newLevel 级！"
+                    ).color(color(0xffea3a))
+                )
+                break
             }
+        }
+        if (oldLevel != newLevel) {
+            xc.changePlayerBalance(p0.uniqueId,p0.name,allCost.toBigDecimal(),false)
+            item.editMeta { it.addEnchant(Enchantment.FEATHER_FALLING, newLevel, true) }
+        }
+        if (oldFood != newFood) {
+            item.editMeta { it.addEnchant(Enchantment.PUNCH, newFood, true) }
         }
         when (item.itemMeta.getEnchantLevel(Enchantment.FEATHER_FALLING)) {
             15 -> item.editMeta { it.setRarity(ItemRarity.UNCOMMON) }
             30 -> item.editMeta { it.setRarity(ItemRarity.RARE) }
             45 -> item.editMeta { it.setRarity(ItemRarity.EPIC) }
         }
-        p0.sendActionBar(text("升级成功！共消耗了 ${xc.getdisplay(allCost.toBigDecimal())} ！").color(color(0x00ff00)))
+        p0.sendActionBar(text("升级共消耗了 ${xc.getdisplay(allCost.toBigDecimal())} ！").color(color(0x00ff00)))
         return true
     }
 
     fun getUpCost(lvl: Int): Double {
-        return lvl.toDouble() + round(Random.nextDouble() * 2000.0) / 100
+        return lvl.toDouble() + round(Random.nextDouble() * 2000.0) / 100 - 5.0
     }
 }
